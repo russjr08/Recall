cc19083dd460cf7850514ca9b14a9db02b915103
Russell Richardson <Russell@KronosAD.com>
2020-01-31 05:52:17 -0500

    Add a DatabaseProps derived interface for React Router Route components

--- src/DatabaseProp.ts
+++ src/DatabaseProp.ts
@@ -1,7 +1,13 @@
-import Database from "./Database";
+import Database from "./Database"
+import { RouteComponentProps } from 'react-router-dom'
+
 
 interface DatabaseProp {
     database: Database
+}
+
+export interface DatabaseRouteProp extends DatabaseProp, RouteComponentProps {
+
 }
 
 export default DatabaseProp
