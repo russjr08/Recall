b5843628b199aa8b3ef11fb8601e2d3797bf78b2
Russell Richardson <Russell@KronosAD.com>
2020-02-24 03:12:00 -0500

    If date is earlier than today, flag as "today"

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -8,9 +8,14 @@ class Util {
         }
     }
 
-    // Today check snippet from https://flaviocopes.com/how-to-determine-date-is-today-javascript/
+    // Today check snippet from https://flaviocopes.com/how-to-determine-date-is-today-javascript/ with extras to return true if earlier date
     static isToday = (someDate: Date) => {
         const today = new Date()
+
+        if(someDate < today) {
+            return true
+        }
+
         return someDate.getDate() === today.getDate() &&
           someDate.getMonth() === today.getMonth() &&
           someDate.getFullYear() === today.getFullYear()
